8b09e5949519dd9c4979a644643e7d357c91f0f3
lcp <lcphutchinson@gmail.com>
2024-01-24 23:02:34 -0500

    db connection snarl resolved

--- src/gl5r/mongoio/db_manager.py
+++ src/gl5r/mongoio/db_manager.py
@@ -3,30 +3,30 @@ from pymongo.server_api import ServerApi
 
 class DBManager(AsyncIOMotorClient):
     
-    def __new__(cls, uri):
+    def __new__(cls, uri : str):
         if not hasattr(cls, 'instance'):
             cls.instance = super(DBManager, cls).__new__(cls)
         return cls.instance
     
-    def __init__(self, uri):
-        super(DBManager, self).__init__()
-        self.db = self.get_database('gl5r')
+    def __init__(self, uri : str):
+        super(DBManager, self).__init__(uri)
+        self.db         = self.get_database('gl5r')
         self.ServerApi  = ServerApi("1")
-        self.uri        = uri
         self.user_cache = dict()       
- 
-    # note: basic server connection confirmed--proper query structure needs building
-    async def ping_server(self):
-        try:
-            self.admin.command('ping')
-            print('ping operation successful')
-        except Exception as e:
-            print(e)
 
     async def get_user(self, user : str):
         if user in self.user_cache:
             return self.user_cache[user]
         else:
-            pass # fetch the user data from the database
-        # note: if the data isn't found, this is a new user. Run an insert to register.
-        # self.user_cache[user] = doc that was fetched from the db.
+            user_id = { '_id': user }
+            try:
+                user_doc = await self.db.users.find_one(user_id)
+                if user_doc:
+                    return user_doc
+                else:
+                    await self.db.users.insert_one(user_id)
+                    self.user_cache[user] = user_id                 # remember to limit cache growth
+                    return user_id
+            except Exception as e:
+                pass # logic for timeouts goes here
+                
